cd60eb362d15dfdf73cad6a552086566c5ca8229
Shivraj Rawat <ShivrajRawat@users.noreply.github.com>
2024-03-25 18:29:18 +0530

    Updated

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,26 +2,35 @@
 importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js")
 importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging.js")
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAh1dJ1YwWTWAmPmqnWYSL0ySNK2Z-WsXk",
-  authDomain: "easyopd-eb7ba.firebaseapp.com",
-  projectId: "easyopd-eb7ba",
-  storageBucket: "easyopd-eb7ba.appspot.com",
-  messagingSenderId: "948563831343",
-  appId: "1:948563831343:web:9a6d45417b31d3b52c34da",
-  measurementId: "G-7C0WFD0CCJ",
-});
+if(firebase){
+  firebase.initializeApp({
+    apiKey: "AIzaSyAh1dJ1YwWTWAmPmqnWYSL0ySNK2Z-WsXk",
+    authDomain: "easyopd-eb7ba.firebaseapp.com",
+    projectId: "easyopd-eb7ba",
+    storageBucket: "easyopd-eb7ba.appspot.com",
+    messagingSenderId: "948563831343",
+    appId: "1:948563831343:web:9a6d45417b31d3b52c34da",
+    measurementId: "G-7C0WFD0CCJ",
+  });
+  
+  const messaging = firebase.messaging();
+  if(messaging){
+    messaging.onBackgroundMessage(function(payload){
+      console.log('[firebase-messaging-sw.js] Received background message ', payload);
+      // Customize notification here
+      const title = 'Background Message Title';
+      const options = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+      };
+    
+      self.registration.showNotification(title, options);
+    });
+  }
+  else{
+    console.log('SW::Firebase messaging is not available.')
+  }
+}
+console.log('SW::Firebase is not initialize.')
 
-const messaging = firebase.messaging();
-messaging.onBackgroundMessage(function(payload){
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const title = 'Background Message Title';
-  const options = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(title, options);
-});
 
